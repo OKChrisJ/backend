94ff89c341a4b0f8b2948f82a5fb0cefd7e225c7
Chris Jeff <chrisjeff429@gmail.com>
2022-11-24 09:03:46 -0500

    testing python commits

--- api/views.py
+++ api/views.py
@@ -1,4 +1,10 @@
 from django.http import JsonResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+
+from api.models import *
+from api.serializers import *
 
 def api_home(request, *args, **kwargs):
     return JsonResponse({"massage": "Hi there, this is your Django api response!"})
