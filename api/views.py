1e207a0ad1c171089a5dc149d2123525310dae87
Chris Jeff <chrisjeff429@gmail.com>
2022-11-25 13:39:16 -0500

    Changes to settings.py

--- api/views.py
+++ api/views.py
@@ -6,5 +6,6 @@ from rest_framework.parsers import JSONParser
 from api.models import *
 from api.serializers import *
 
+@csrf_exempt
 def api_home(request, *args, **kwargs):
     return JsonResponse({"message": "Hi there, this is your Django api response!"})
--- cfehome/settings.py
+++ cfehome/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i&cww(j(+^22_&o=4h3c$p%)80d6w5w78fm@#(4ms2)ej^pv7@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
